5d82058a7d001fc7d49a3a667bcf0c3a89241c52
Cesar Augusto Barros <cesar@parana.com.br>
2019-07-15 11:00:25 -0300

    atualização

--- CaixaEletronico/CaixaEletronico/Principal.cs
+++ CaixaEletronico/CaixaEletronico/Principal.cs
@@ -6,12 +6,16 @@ namespace CaixaEletronico
     {
         static void Main(string[] args)
         {
-            //valores
-            int cinquenta = 0;
-            int vinte = 0;
-            int dez = 0;
+            //Variaveis para armazenar o que tem em caixa
+            var cofre = new Saldo();
+            //Variaveis para calculo do saque
+            int saque;
+            int sCinquenta;
+            int sVinte;
+            int sDez;
+            int resto;
             //para armazenar o que tem no caixa
-            int saldo = 0;
+            //int saldo = 0;
 
             int op = 0;
             do
@@ -34,22 +38,39 @@ namespace CaixaEletronico
                 {
                     case 1:
                         Console.WriteLine("Insira a quantidade de notas de 50 reais");
-                        cinquenta = Convert.ToInt32(Console.ReadLine());
+                        cofre.addCinquenta((Convert.ToInt32(Console.ReadLine())));
                         break;
                     case 2:
                         Console.WriteLine("Insira a quantidade de notas de 20 reais");
-                        vinte = Convert.ToInt32(Console.ReadLine());
+                        cofre.addVinte(Convert.ToInt32(Console.ReadLine()));
                         break;
                     case 3:
                         Console.WriteLine("Insira a quantidade de notas de 10 reais");
-                        dez = Convert.ToInt32(Console.ReadLine());
+                        cofre.addDez(Convert.ToInt32(Console.ReadLine()));
                         break;
                     case 4:
-
+                        Console.WriteLine("Qual o valor desejado:");
+                        saque = Convert.ToInt32(Console.ReadLine());
+                        //Calcular quantidade de notas
+                        sCinquenta = saque / 50;
+                        resto = saque - (sCinquenta * 50);
+                        sVinte = resto / 20;
+                        resto = resto - (sVinte * 20);
+                        sDez = resto / 10;
+                        resto = resto - (sDez * 10);
+                        //imprimir a quantidade de notas
+                        Console.WriteLine("Será liberado:");
+                        Console.WriteLine("-" + sCinquenta + "notas de cinquenta reais");
+                        Console.WriteLine("-" + sVinte + "notas de vinte reais");
+                        Console.WriteLine("-" + sDez + "notas de dez reais");
+                        //Atualizar saldo do caixa eletronico
+                        //cofre.addCinquenta -= sCinquenta;
+                        //cofre.vinte -= sVinte;
+                        //cofre.dez -= sDez;
                         break;
-                    case 5:
-                        saldo = (cinquenta * 50) + (vinte * 20) + (dez * 20);
-                        Console.WriteLine("Saldo de R$" + saldo + ",00");
+                    case 5:                        
+                        //saldo = (cinquenta * 50) + (vinte * 20) + (dez * 20);
+                        Console.WriteLine(cofre.notasCinquenta);
                         break;
                     default:
                         break;
---
+++ CaixaEletronico/CaixaEletronico/Saldo.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CaixaEletronico
+{
+    class Saldo
+    {
+        private int _cinquenta;
+        private int _vinte;
+        private int _dez;
+        private int _saldo;
+
+        //verificar quantidade de notas
+        public int notasCinquenta {
+            get { return this._cinquenta; }
+        }
+        public int notasVinte
+        {
+            get { return this._vinte; }
+        }
+        public int notasDez
+        {
+            get { return this._dez; }
+        }
+
+        public int saldo {
+            get { return this._saldo; }
+        }
+
+        //depositar notas
+        public void addCinquenta (int valor)
+        {
+            this._cinquenta += valor;
+        }
+        public void addVinte (int valor)
+        {
+            this._vinte += valor;
+        }
+        public void addDez (int valor)
+        {
+            this._dez += valor;
+        }
+
+        //retirarnotas
+        public void retiraCinquenta(int valor)
+        {
+            this._cinquenta -= valor;
+        }
+        public void retiraVinte(int valor)
+        {
+            this._vinte -= valor;
+        }
+        public void retiraDez(int valor)
+        {
+            this._dez -= valor;
+        }
+
+        public void calculaSaldo()
+        {
+            this._saldo = (this._cinquenta * 50) + (this._vinte * 20) + (this._dez * 10);
+        }
+    }
+}
+
