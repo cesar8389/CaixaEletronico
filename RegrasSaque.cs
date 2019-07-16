bd20666201be60409f16fb9e07ff1f60b91bd795
cesar8389 <cesar8389@outlook.com>
2019-07-16 20:56:40 -0300

    0.0.2

--- RegrasSaque.cs
+++ RegrasSaque.cs
@@ -4,7 +4,7 @@ namespace CaixaEletronico
 {
     class RegrasSaque
     {
-        String a;
+        
         public String Saque(int saque, int cinquenta, int vinte, int dez)
         {
             int saldo = (cinquenta * 50) + (vinte * 20) + (dez * 10);
@@ -12,36 +12,40 @@ namespace CaixaEletronico
             int sacaCinquenta = 0;
             int sacaVinte = 0;
             int sacaDez = 0;
-            if (saque % 10 > 0 || saque > saldo)
+
+            if (saque % 10 == 0 && saque <= saldo)
             {
-                while (resto % 10 == 0)
+                while (cinquenta >= 0 && resto >= 50)
                 {
-                    while (cinquenta >= 0 && resto >= 50)
-                    {
-                        resto -= 50;
-                        cinquenta -= 1;
-                        sacaCinquenta += 1;
-                    }
+                    resto -= 50;
+                    cinquenta -= 1;
+                    sacaCinquenta += 1;
                     while (vinte >= 0 && resto >= 20)
                     {
                         resto -= 20;
                         vinte -= 1;
                         sacaVinte += 1;
+                        while (dez >= 0 && resto >= 20)
+                        {
+                            resto -= 10;
+                            dez -= 1;
+                            sacaDez += 1;
+                        }
                     }
-                    while (dez >= 0 && resto >= 20)
-                    {
-                        resto -= 10;
-                        dez -= 1;
-                        sacaDez += 1;
-                    }
-                    a = "Seu dinheito sera entregue:\n" +
-                               $"{sacaCinquenta} notas de 50 reais" +
-                               $"{sacaVinte} notas de 20 reais" +
-                               $"{sacaDez} notas de 10 reais";
                 }
+                
+                
+                
+                
             }
-            else { a = "Não há notas disponiveis para atender seu pedido"; }
-            return a;
+            if (resto != 0)
+            {
+                return "Seu dinheito sera entregue:\n" +
+                        $"{sacaCinquenta} notas de 50 reais\n" +
+                        $"{sacaVinte} notas de 20 reais\n" +
+                        $"{sacaDez} notas de 10 reais\n"; ;
+            }
+            else { return "Não há notas disponiveis para esse saque"; }
         }
     }
 }
